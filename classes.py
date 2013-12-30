57610829bb8443500f9e77a93e5285325735c0c1
Han Xu <hgcrpd@gmail.com>
2013-12-30 08:11:38 -0800

    WIP setting up classes still

--- classes.py
+++ classes.py
@@ -1,16 +1,31 @@
 """
 Classes for Sleigh packing problem.
 """
+MAX_X = 1000
+MAX_Y = 1000
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Present(object):
     """
     A Present to be packed in the sleigh
     """
-    def __init__(self, dim1, dim2, dim3):
-        self.dim1 = dim1  # "X" without rotation
-        self.dim2 = dim2  # "Y" without rotation
-        self.dim3 = dim3  # "Z" without rotation
+    def __init__(self, pid, dim1, dim2, dim3):
+        self.pid = int(pid)
+        self.x = int(dim1)  # "X" without rotation
+        self.y = int(dim2)  # "Y" without rotation
+        self.z = int(dim3)  # "Z" without rotation
+
+    def __unicode__(self):
+        return "Present: {}, {}, {}".format(self.x, self.y, self.z)
+
+    def get_opposite_corner(self, x1, y1, z1=1):
+        x2 = x1 + self.x - 1
+        y2 = y1 + self.y - 1
+        z2 = z1 + self.z - 1
+        return x2, y2, z2
 
     def get_vertices(self, x1, y1, z1):
         """
@@ -25,9 +40,7 @@ class Present(object):
                            x2 y1 z2
                            x2 y2 z2
         """
-        x2 = x1 + int(self.dim1) - 1
-        y2 = y1 + int(self.dim2) - 1
-        z2 = z1 + int(self.dim3) - 1
+        x2, y2, z2 = self.get_opposite_corner(x1, y1, z1)
         list_vertices = [
             [x1, y1, z1],
             [x1, y2, z1],
@@ -56,18 +69,69 @@ class Layer(object):
         self.max_x = 1
         self.max_y = 1
         self.max_z = z
-        self.presents = []
+        # Cursor object
+        self.cursor = LayerCursor()
+        # Keys are (x, y, z) coordinates for the Present, values are the Present object
+        self.presents = {}
+
+    def place_present(self, present):
+        """
+        - Start at (1,1,1). Pack along y=1, z=1 until full
+        - Move y = max_occupied_y. Pack until full
+        - Return False if present doesn't fit on this Layer
+        """
+        logger.info("Placing present: {}".format(present))
+        x2, y2, z2 = present.get_opposite_corner(self.cursor.x, self.cursor.y, self.z)
+
+        if x2 > MAX_X:
+            logger.info("Present doesn't fit in row, starting new row")
+            # If it exceeds the MAX_X coordinate, reset cursor to new row in layer and re-position the present
+            self.cursor.x = 1
+            self.cursor.y = self.max_y
+            x2, y2, z2 = present.get_opposite_corner(self.cursor.x, self.cursor.y, self.z)
+
+        if y2 > MAX_Y:
+            # If it exceeds the MAX_Y coordinate, then return False to indicate Present doesn't fit
+            logger.info("Present doesn't fit in Layer")
+            return False
+
+        # If we can place it, add the Present to the hash
+        logger.info("Placing present at {}, {}".format(self.cursor.x, self.cursor.y))
+        self.presents[(self.cursor.x, self.cursor.y)] = present
+        # Update the max coordinates
+        if x2 > self.max_x:
+            self.max_x = x2
+        if y2 > self.max_y:
+            self.max_y = y2
+        if z2 > self.max_z:
+            self.max_z = z2
+
+        # Update the cursor
+        self.cursor.x = self.max_x
+        return True
+
+
+class LayerCursor(object):
+    """
+    Cursor object for keeping track of where we are in a layer
+    """
+    def __init__(self):
+        self.x = 1
+        self.y = 1
 
 
 class Sleigh(object):
     """
     A Sleigh is a collection of Layers
     """
-    MAX_X = 1000
-    MAX_Y = 1000
-
     def __init__(self):
-        self.layers = []
+        # Hash of layers
+        # Keys are z coordinates of the layer, values are the Layer object
+        self.layers = {}
+        self.max_z = 1
+
+    def get_new_layer(self):
+        pass
 
     def score(self):
         pass
--- run.py
+++ run.py
@@ -1,15 +1,36 @@
 """
 Packing algorithms
 """
-
+import csv
+import os
 import classes
+import logging
 
 
-def sample_bottom_up():
+def create_header():
+    header = ['PresentId']
+    for i in xrange(1,9):
+        header += ['x' + str(i), 'y' + str(i), 'z' + str(i)]
+    return header
+
+
+def sample_bottom_up(outfile='sub_bottomup_1.csv'):
     """
     Replicate the sample bottom-up approach
     """
-    pass
+    sleigh = classes.Sleigh()
+
+    presents_file = './data/presents_revorder.csv'
+    outfile = os.path.join('data', outfile)
+    with open(presents_file, 'rb') as presents:
+        with open(outfile, 'wb') as out:
+            presents.readline() # skip header
+            read = csv.reader(presents)
+            write = csv.writer(out)
+            write.writerow(create_header())
+            for row in read:
+                present = classes.Present(*row)
+                pass
 
 
 def sample_top_down():
